df687d698f7a7d1387c18b55cd000349bc99385b
DlopezS98 <01dlopezs98@gmail.com>
2024-07-12 00:12:44 -0600

    Update appsettings json

--- Models/AppSettings.cs
+++ Models/AppSettings.cs
@@ -8,5 +8,13 @@ public class AppSettings
 public class Services
 {
     public const string SectionName = "Services";
-    public string PetClinicBaseApiUrl { get; set; } = string.Empty;
+    public PetClinic PetClinic { get; set; } = new();
+}
+
+public class PetClinic
+{
+    public const string SectionName = "PetClinic";
+    public string BaseUrl { get; set; } = string.Empty;
+    public string Vets { get; set; } = string.Empty;
+    public string Owners { get; set; } = string.Empty;
 }
